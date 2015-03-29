268cdf6652ab02ad358e0d9e714c38e60c2f08f0
zhushh <zsh5318@gmail.com>
2015-03-29 16:43:59 +0800

    change some details

--- Group3/13331381/S2/ajax.js
+++ Group3/13331381/S2/ajax.js
@@ -18,116 +18,116 @@ window.onload = function () {
 var reset = function () {
     var bts = document.getElementsByClassName("button");
     var numbers = document.getElementsByClassName("number");
-    var ans = document.getElementById("answer");
-    var info_bar = document.getElementById("info-bar");
-
-    info_bar.status = "unactive";
-    ans.style.display = "none";
-    ans.innerHTML = "";
-    var l = numbers.length;
-    for (var j = 0; j < l; j = j + 1) {
-        numbers[j].style.display = "none";
-        numbers[j].innerHTML = "...";
-        bts[j].style.background = "rgba(48, 63, 159, 1)";
-    }
-    listener(bts);
-}
-
-function listener(bts) {
-    var l = bts.length;
-    for (var i = 0; i < l; i = i + 1) {
-        bts[i].status = "active";
-        bts[i].onclick = function (index, cur) {
+    for (var i = 0; i < bts.length; i = i + 1) {
+        numbers[i].style.display = "none";
+        numbers[i].innerHTML = "...";
+        bts[i].style.background = "rgba(48, 63, 159, 1)";
+        bts[i].onclick = function (index, bts) {
             return function() {
-                if (cur.status == "active") {
-                    cur.status = "unactive";
-                    clearEventListeners(index);
-                    setNoActiveCssStyle(index);
-                    ajax_require(index);
-                }
-            }
-        }(i, bts[i]);
+                bts[index].onclick = null;
+                clearEventListeners(index, bts);
+                setNoActiveCssStyle(index);
+                ajax_require(index, bts);
+            };
+        }(i, bts);
     }
-}
+    var info_bar = document.getElementById("info-bar");
+    info_bar.onclick = null;
+    var answer = document.getElementById("answer");
+    answer.style.display = "none";
+};
 
-var clearEventListeners = function (i) {
-    var bts = document.getElementsByClassName("button");
+var clearEventListeners = function (i, bts) {
     var l = bts.length;
     for (var j = 0; j < l; j = j + 1) {
+        bts[j].onclick = null;  // set other button unactive
+    }
+};
+
+var activeEventListers = function (i, bts) {
+    var l = bts.length;
+    for (var j in bts) {
         if (j != i) {
-            bts[j].status = "unactive";     // set other button unactive
+            bts[j].onclick = function (index, bts) {
+                return function() {
+                    bts[index].onclick = null;
+                    clearEventListeners(index, bts);
+                    setNoActiveCssStyle(index);
+                    ajax_require(index, bts);
+                };
+            }(j, bts);
         }
     }
-}
+};
+
+var setActiveCssStyle = function () {
+    var numbers = document.getElementsByClassName("number");
+    var l = numbers.length;
+    for (var j = 0; j < l; j = j + 1) {
+        if (numbers[j].innerHTML == "...") {
+            numbers[j].parentNode.style.background = "rgba(48, 63, 159, 1)";
+        } else {
+            numbers[j].parentNode.style.background = "rgb(150,150,150)";
+        }
+    }
+};
 
 var setNoActiveCssStyle = function (i) {
     var numbers = document.getElementsByClassName("number");
     var l = numbers.length;
-    numbers[i].style.display = "block";     // show waiting number
+    if (i < l) {
+        numbers[i].style.display = "block";     // show waiting number status
+    }
     for (var j = 0; j < l; j = j + 1) {
-        if (j != i && numbers[j].innerHTML == "...") {
+        if (j != i) {
             numbers[j].parentNode.style.background = "rgb(150,150,150)";
         }
     }
-}
+};
 
-var ajax_require = function (i) {
+var ajax_require = function (i, bts) {
     var xmlhttp;
     if (window.XMLHttpRequest) {
         xmlhttp = new XMLHttpRequest();
     } else {
         xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {     // get number successfully
-            var numbers = document.getElementsByClassName("number");
-            numbers[i].innerHTML = xmlhttp.responseText;
-            reActiveEventListeners(i);
-            count_answer();
-        }
-    }
-    xmlhttp.open("GET", "./", true);
-    xmlhttp.send();
-}
-
-var reActiveEventListeners = function (i) {
-    var bts = document.getElementsByClassName("button");
-    var numbers = document.getElementsByClassName("number");
-    var l = numbers.length;
-    for (var j = 0; j < numbers.length; j = j + 1) {    // set button css style
-        if (j == i) {
-            numbers[j].parentNode.style.background = "rgb(150,150,150)";
-        } else if (numbers[j].innerHTML == "...") {
-            numbers[j].parentNode.style.background = "rgba(48, 63, 159, 1)";
-            bts[j].status = "active";
-        }
-    }
-}
-
-function count_answer() {
-    var ans = document.getElementById("answer");
-    if (ans.innerHTML == "") {
-        var numbers = document.getElementsByClassName("number");
-        var l = numbers.length;
-        var sum = 0;
-        var count = 0;
-        for (var j = 0; j < l; j = j + 1) {
-            if (numbers[j].innerHTML != "...") {
-                count = count + 1;
-                sum += parseInt(numbers[j].innerHTML);
+    xmlhttp.onreadystatechange = function(index, bts, xmlhttp) {
+        return function() {     // get number successfully
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                var numbers = document.getElementsByClassName("number");
+                numbers[index].innerHTML = xmlhttp.responseText;
+                activeEventListers(index, bts);
+                setActiveCssStyle();
+                determineIfShouldShowAnswer();
             }
         }
-        if (count == l) {
-            var info_bar = document.getElementById("info-bar");
-            info_bar.onclick = function(ans, s, ib) {
-                return function() {
-                    if (ib.status == "unactive") {
-                        ib.status = "active";
-                        ans.style.display = "block";
-                        ans.innerHTML = s.toString();
-                    }
-                };
-            }(ans, sum, info_bar);
+    }(i, bts, xmlhttp);
+    xmlhttp.open("GET", "./", true);
+    xmlhttp.send();
+};
+
+var determineIfShouldShowAnswer = function () {
+    var OK = true;
+    var sum = 0;
+    var numbers = document.getElementsByClassName("number");
+    for (var j = 0; j < numbers.length; j = j + 1) {
+        if (numbers[j].innerHTML == "...") {
+            OK = false;
+            break;
+        } else {
+            sum += parseInt(numbers[j].innerHTML);
         }
+    }
+    if (OK) {
+        var info_bar = document.getElementById("info-bar");
+        info_bar.onclick = function (s, info_bar) {
+            return function () {
+                info_bar.onclick = null;
+                var answer = document.getElementById("answer");
+                answer.innerHTML = "" + s;
+                answer.style.display = "block";
+            };
+        }(sum, info_bar);
     }
 }
